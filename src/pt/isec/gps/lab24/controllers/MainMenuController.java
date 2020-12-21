fb3bb00ad05ba81ce34ba465d5ecf91ff0a8ebd5
AngeloGSilva <47340682+AngeloGSilva@users.noreply.github.com>
2020-12-21 19:35:00 +0000

    Ver se a dificuldade esta marcada

--- src/pt/isec/gps/lab24/controllers/MainMenuController.java
+++ src/pt/isec/gps/lab24/controllers/MainMenuController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.RadioButton;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 import pt.isec.gps.lab24.DialogModal;
--- src/pt/isec/gps/lab24/controllers/NovoJogoController.java
+++ src/pt/isec/gps/lab24/controllers/NovoJogoController.java
@@ -1,13 +1,24 @@
 package pt.isec.gps.lab24.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import pt.isec.gps.lab24.Commons;
 import pt.isec.gps.lab24.modal.Jogador;
 
+import javax.swing.*;
+
 public class NovoJogoController {
+    @FXML
+    private RadioButton facil;
+    @FXML
+    private RadioButton normal;
+    @FXML
+    private RadioButton dificil;
+
     private Jogador jogador;
-
-
 
     public Jogador getJogador() {
         return jogador;
@@ -18,8 +29,25 @@ public class NovoJogoController {
     }
 
     public void voltar(ActionEvent event) {
-        new Commons().mudarEcra(Commons.VIEW_MAIN_MENU, event, jogador);
+            new Commons().mudarEcra(Commons.VIEW_MAIN_MENU, event, jogador);
+        }
+
+
+    public void jogar(ActionEvent event) {
+        if(check()== true) {
+        new Commons().mudarEcra(Commons.VIEW_TABULEIRO,event,jogador);}
     }
 
-    public void jogar(ActionEvent event) {new Commons().mudarEcra(Commons.VIEW_TABULEIRO,event,jogador);}
+    public boolean check(){
+        if(facil.isSelected() == false && normal.isSelected() == false && dificil.isSelected() == false){
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Nada Selecionado");
+            alert.setHeaderText(null);
+            alert.setContentText("Precisa de selecionar uma dificuldade");
+            alert.showAndWait();
+            return false;}
+        return true;
+
+    }
+
 }
