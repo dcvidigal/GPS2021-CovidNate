578333747203d6d285be4b5e69bb96d53edf183f
Yamir Carvalho <a21240091@isec.com>
2021-01-04 16:20:23 +0000

    Testes iniciais

---
+++ src/pt/isec/gps/lab24/testes/PessoaTest.java
@@ -0,0 +1,38 @@
+package pt.isec.gps.lab24.testes;
+
+import org.junit.jupiter.api.Test;
+
+class PessoaTest {
+
+    @Test
+    void tentaFicarImune() {
+    }
+
+    @Test
+    void setQuarentena() {
+    }
+
+    @Test
+    void setTurnosEmQuarentena() {
+    }
+
+    @Test
+    void decTurnoEmIsolamento() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void setInfetada() {
+    }
+
+    @Test
+    void contactoPessoaInfetada() {
+    }
+
+    @Test
+    void resetContacto() {
+    }
+}
---
+++ src/pt/isec/gps/lab24/testes/TabuleiroTest.java
@@ -0,0 +1,26 @@
+package pt.isec.gps.lab24.testes;
+
+import org.junit.jupiter.api.Test;
+
+class TabuleiroTest {
+
+    @Test
+    void proximoTurno() {
+    }
+
+    @Test
+    void isFimJogo() {
+    }
+
+    @Test
+    void getPessoa() {
+    }
+
+    @Test
+    void getNumInfetadosInicial() {
+    }
+
+    @Test
+    void decNumInfetados() {
+    }
+}
